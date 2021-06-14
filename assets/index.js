3cf094b9daf63e769c4d21ebfbc031b0fbd18ade
Elijah Miernicki <miernickielijah@gmail.com>
2021-06-14 13:49:11 -0500

    added links that pop up on click

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,9 @@
-function openNav() {
-
+//display NAV on click//
+function change() {
+    const elements = document.querySelectorAll("li.hide");
+    for (let element of elements) {
+        element.classList.toggle("transformed");
+    }
 }
 
-function closeNav() {
-
-}
+$('#explore').click(change);
